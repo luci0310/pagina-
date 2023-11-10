9281402974e3218bb8f6461a4962a5b74c6b1639
luci0310 <lucianamartinez898@gmail.com>
2023-11-10 00:49:45 -0300

    subo el navbar sin personalizar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
+//importaciones del navbar 
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    MatIconModule, 
+    MatButtonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    NavbarComponent,
+    CommonModule,
+    SharedRoutingModule,
+    MatIconModule, 
+    MatButtonModule,
+    MatToolbarModule
   ]
 })
 export class SharedModule { }
